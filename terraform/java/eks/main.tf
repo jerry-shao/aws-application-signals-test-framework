c3ff424d50eb82e59f65fe72fc6899173afcab07
Mingjie Shao <mingjs@amazon.com>
2024-07-15 23:16:54 +0100

    Test credential

--- terraform/java/eks/main.tf
+++ terraform/java/eks/main.tf
@@ -121,7 +121,8 @@ resource "kubernetes_deployment" "sample_app_deployment" {
           }
           env {
             name = "RDS_MYSQL_CLUSTER_PASSWORD"
-            value = var.rds_mysql_cluster_password
+            value = ">6TjH%v0qnW)9M|Qe0$$(<9viA$E"
+#             value = var.rds_mysql_cluster_password
           }
           port {
             container_port = 8080
